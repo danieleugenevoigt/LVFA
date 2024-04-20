a395ca755a79be4c7aff4ce127fcd393cdcb1b1f
danieleugenevoigt <dvoigt@tutanota.com>
2024-04-20 16:31:39 -0600

    added about component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import { React } from 'react';
 import './App.css';
 import Header from './components/header/ Header';
 import Footer from './components/footer/Footer';
+import About from './components/about/About';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
-        <div className="contentContainer" />
+        <div className="contentContainer">
+          <About />
+        </div>
         <Footer />
       </header>
     </div>
---
+++ src/components/about/About.js
@@ -0,0 +1,16 @@
+/* eslint-disable max-len */
+import React from 'react';
+import './about.css';
+
+function About() {
+  return (
+    <div className="about">
+      <h2>The Process</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet temporibus fugit dolores provident voluptatum assumenda voluptatem quod soluta. Error dolor soluta dignissimos dolore explicabo voluptatum veniam ipsa odit blanditiis adipisci, sequi nostrum, deleniti, similique laboriosam. Nobis amet ipsum esse incidunt perferendis fugiat cupiditate, culpa beatae, nostrum pariatur excepturi. Consequuntur molestias culpa alias dicta reiciendis unde dolorem rem qui molestiae recusandae autem pariatur mollitia totam tempora repellat quos deleniti assumenda et voluptas aliquam, excepturi expedita quia distinctio. Corporis, necessitatibus saepe. Consequatur.
+      </p>
+    </div>
+  );
+}
+
+export default About;
