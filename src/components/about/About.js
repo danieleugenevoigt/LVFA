94530b9f062653e71153d0fb02f1ffdd21ac5414
danieleugenevoigt <dvoigt@tutanota.com>
2024-04-21 00:24:06 -0600

    added about text and styling

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,7 +7,15 @@ function About() {
     <div className="about">
       <h2>The Process</h2>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet temporibus fugit dolores provident voluptatum assumenda voluptatem quod soluta. Error dolor soluta dignissimos dolore explicabo voluptatum veniam ipsa odit blanditiis adipisci, sequi nostrum, deleniti, similique laboriosam. Nobis amet ipsum esse incidunt perferendis fugiat cupiditate, culpa beatae, nostrum pariatur excepturi. Consequuntur molestias culpa alias dicta reiciendis unde dolorem rem qui molestiae recusandae autem pariatur mollitia totam tempora repellat quos deleniti assumenda et voluptas aliquam, excepturi expedita quia distinctio. Corporis, necessitatibus saepe. Consequatur.
+        In my artistic process, I begin with the raw material of feeling, emotion, or resonant words. These intangible sparks ignite the creative journey, guiding me through the digital realm where I craft my initial vision.
+
+        Utilizing modern software tools, I refine this vision, shaping it into a digital painting that captures the essence of my inspiration. Here, meticulous attention is paid to color palettes, layering for depth and tone, and fine-tuning details to ensure the integrity of the piece.
+
+        Yet, it is in the transition from digital to physical that the true essence of my work emerges. Employing the precision of CNC laser cutting, I translate the digital canvas into tangible layers of paper, each piece a fragment waiting to be assembled.
+
+        Hand-assembling each layer, I imbue the work with a personal touch, occasionally adding or subtracting details to enhance the piece&aposs coherence. This process of deconstruction and reconstruction allows for a deeper engagement with the work, fostering a tangible connection between creator and creation.
+
+        In this synthesis of modern technology and traditional craftsmanship, I find fulfillment in the act of creation. It is a process that honors both the innovation of digital tools and the timeless beauty of paper as a medium of expression. Through this journey, I discover not only the essence of my art but also a deeper understanding of myself as an artist.
       </p>
     </div>
   );
