b72e94c577f1659b37cf2a577e3a3f0253b045e5
daniel <daniel@pop-os.localdomain>
2024-04-22 13:32:35 -0600

    added anchors/routing to pages in nav

--- src/components/header/ Header.js
+++ src/components/header/ Header.js
@@ -6,11 +6,42 @@ function Header() {
     <div className="headerMain">
       <h1>Lauren Voigt Fine Art</h1>
       <ul className="navMenu">
-        <li>Gallery</li>
-        <li>About</li>
-        <li>News</li>
-        <li>Events</li>
-        <li>Contact</li>
+        <li>
+          <a href="/">
+            Home
+            {' '}
+          </a>
+        </li>
+        <li>
+          <a href="/gallery">
+            Gallery
+            {' '}
+          </a>
+        </li>
+        <li>
+          <a href="/about">
+            About
+            {' '}
+          </a>
+        </li>
+        <li>
+          <a href="/news">
+            News
+            {' '}
+          </a>
+        </li>
+        <li>
+          <a href="/events">
+            Events
+            {' '}
+          </a>
+        </li>
+        <li>
+          <a href="/contact">
+            Contact
+            {' '}
+          </a>
+        </li>
       </ul>
     </div>
   );
