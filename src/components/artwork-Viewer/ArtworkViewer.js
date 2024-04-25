cfc40c4969e6d15bb1c42cd53dc05b22f98fba97
daniel <daniel@pop-os.localdomain>
2024-04-25 15:44:25 -0600

    reset branch to beginning

--- src/components/artwork-Viewer/ArtworkViewer.js
+++ src/components/artwork-Viewer/ArtworkViewer.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import './artworkViewer.css';
-import image1 from '../../images/1.jpg';
-import image2 from '../../images/2.jpg';
-import image3 from '../../images/3.jpg';
+import ProductView from '../product-view/ProductView';
 
 function ArtworkViewer() {
   return (
     <div className="artworkViewerContainer">
-      <div>
-        <img src={image1} alt="artwork-series-1" />
-        <img src={image2} alt="artwork-series-2" />
-        <img src={image3} alt="artwork-series-3" />
-      </div>
+      <ProductView />
     </div>
   );
 }
--- src/components/product-view/ProductView.js
+++ src/components/product-view/ProductView.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ProductView() {
   return (
     <div>
-      <ProductView />
+      product view
     </div>
   );
 }
