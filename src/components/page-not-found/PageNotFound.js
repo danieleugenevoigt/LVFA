6eaecca97fbd220a04ca2f0fb39d03a0a5874458
daniel <daniel@pop-os.localdomain>
2024-04-26 10:22:05 -0600

    basic 360 viewer working

--- src/components/page-not-found/PageNotFound.js
+++ src/components/page-not-found/PageNotFound.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './pageNotFound.css';
-import image1 from '../../images/pnf.png';
 
 function PageNotFound() {
   return (
     <div className="pageNotFound">
       <h1>Page Not Found</h1>
-      <img src={image1} alt="pic" />
+      <img src={`${process.env.PUBLIC_URL}/images/pnf.png`} alt="Not Found Magnifying Glass" />
     </div>
   );
 }
--- src/components/product-view/ProductView.js
+++ src/components/product-view/ProductView.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function ProductView() {
+export default function SimpleSlider() {
+  const [value, setValue] = useState(0);
+  const [number, setNumber] = useState(1);
+  // const imagePath = '../../images';
+  // {`${imagePath}/car-${number}.png`}
+  // {`${process.env.PUBLIC_URL}/images/car-${number}.png`}
+
+  const handleChange = (event) => {
+    const newValue = parseInt(event.target.value, 10);
+    let newData = Math.round((newValue * (36 / 101)));
+    if (newData === 0) {
+      newData = 1;
+    }
+    setNumber(newData);
+    setValue(newValue);
+  };
+
   return (
     <div>
-      product view
+      <h2>360 Image View</h2>
+      <img src={`${process.env.PUBLIC_URL}/images/car-${number}.png`} width="800" height="400" alt={`Car ${number}`} />
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <span>Slide to view different angles:</span>
+        <input
+          type="range"
+          min="0"
+          max="102"
+          value={value}
+          onChange={handleChange}
+          style={{ width: '300px', margin: '0 10px' }}
+        />
+        <span>{value}</span>
+      </div>
     </div>
   );
 }
-
-export default ProductView;
