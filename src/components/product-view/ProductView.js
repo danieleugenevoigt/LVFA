2c92e5272f735c729d8eb2020c8817492a3de9de
danieleugenevoigt <dvoigt@tutanota.com>
2024-04-27 11:36:31 -0600

    adjust slider styling and control

--- src/components/product-view/ProductView.js
+++ src/components/product-view/ProductView.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 import './productView.css';
 
 export default function SimpleSlider() {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(50);
   const [number, setNumber] = useState(1);
   const title = 'Sample art title 12" by 12" 2023';
 
   const handleChange = (event) => {
     const newValue = parseInt(event.target.value, 10);
-    let newData = Math.round((newValue * (36 / 101)));
+    let newData = Math.round((newValue * (36 / 100)));
     if (newData === 0) {
       newData = 1;
     }
@@ -23,9 +23,10 @@ export default function SimpleSlider() {
         <div className="slider">
           <span>View Angle:</span>
           <input
+            className="sliderSetup"
             type="range"
-            min="0"
-            max="102"
+            min="1"
+            max="100"
             value={value}
             onChange={handleChange}
           />
