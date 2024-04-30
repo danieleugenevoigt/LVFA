67ccc4e63e9e5e798da60b5d3e672664da88fdba
daniel <daniel@pop-os.localdomain>
2024-04-30 16:21:24 -0600

    added timelapse video to about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,7 +1,6 @@
 /* eslint-disable max-len */
 import React from 'react';
 import './about.css';
-import VideoLoop from '../video-loop/VideoLoop';
 
 function About() {
   return (
@@ -12,12 +11,18 @@ function About() {
         Utilizing modern software tools, I refine this vision, shaping it into a digital painting that captures the essence of my inspiration. Here, meticulous attention is paid to color palettes, layering for depth and tone, and fine-tuning details to ensure the integrity of the piece.
         Yet, it is in the transition from digital to physical that the true essence of my work emerges.
       </p>
-      <VideoLoop />
       <p>
         Employing the precision of CNC laser cutting, I translate the digital canvas into tangible layers of paper, each piece a fragment waiting to be assembled.
         Hand-assembling each layer, I imbue the work with a personal touch, occasionally adding or subtracting details to enhance the piece&apos;s coherence. This process of deconstruction and reconstruction allows for a deeper engagement with the work, fostering a tangible connection between creator and creation.
         In this synthesis of modern technology and traditional craftsmanship, I find fulfillment in the act of creation. It is a process that honors both the innovation of digital tools and the timeless beauty of paper as a medium of expression. Through this journey, I discover not only the essence of my art but also a deeper understanding of myself as an artist.
       </p>
+      <div className="videoContainer">
+        <video autoPlay loop muted style={{ transform: 'rotate(90deg)' }}>
+          <source src={`${process.env.PUBLIC_URL}/videos/lvfa-timelapse-trapped-2.mp4`} alt="timelapse video" type="video/mp4" />
+          {/* Add additional <source> tags for other video formats like webm, ogg, etc. */}
+          Your browser does not support the video tag.
+        </video>
+      </div>
     </div>
   );
 }
--- src/components/video-loop/VideoLoop.js
+++ src/components/video-loop/VideoLoop.js
@@ -4,8 +4,8 @@ import './videoLoop.css';
 function VideoLoop() {
   return (
     <div className="videoContainer">
-      <video autoPlay loop muted>
-        <source src="path_to_your_video_file.mp4" type="video/mp4" />
+      <video autoPlay loop muted style={{ transform: 'rotate(90deg)' }}>
+        <source src={`${process.env.PUBLIC_URL}/videos/lvfa-timelapse-trapped-2.mp4`} alt="timelapse video" type="video/mp4" />
         {/* Add additional <source> tags for other video formats like webm, ogg, etc. */}
         Your browser does not support the video tag.
       </video>
