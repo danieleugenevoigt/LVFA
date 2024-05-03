bf9a84ffeb74c7853f3cb6c193d6b097bb78b418
daniel <daniel@pop-os.localdomain>
2024-05-03 10:59:05 -0600

    home page slideshow fading complete

--- src/components/slideshow/Slideshow.js
+++ src/components/slideshow/Slideshow.js
@@ -1,26 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
-import './slideshow.css'; // Assuming you have CSS for styling
+import './slideshow.css';
 
 function Slideshow({ imageUrls }) {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const slideshowRef = useRef(null);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % imageUrls.length);
-    }, 3000); // Change interval duration as needed (3000 ms = 3 seconds)
+    }, 5000); // Change interval duration as needed (5000 ms = 5 seconds)
 
     return () => clearInterval(intervalId); // Cleanup interval on component unmount
-  }, [imageUrls.length]); // Re-run effect if imageUrls length changes
+  }, [imageUrls]); // Re-run effect if imageUrls change
+
+  // eslint-disable-next-line consistent-return
+  useEffect(() => {
+    if (slideshowRef.current) {
+      slideshowRef.current.classList.remove('fade-out');
+      slideshowRef.current.classList.add('fade-in');
+
+      // Set timeout to trigger fade-out after a delay
+      const timeoutId = setTimeout(() => {
+        slideshowRef.current.classList.remove('fade-in');
+        slideshowRef.current.classList.add('fade-out');
+
+        // Reset currentIndex after fade-out animation completes
+        setTimeout(() => {
+          setCurrentIndex((prevIndex) => (prevIndex + 1) % imageUrls.length);
+        }, 2000); // Wait for fade-out animation duration (1000ms = 1 second)
+      }, 2000); // Wait before starting fade-out (3000ms = 3 seconds)
+
+      return () => clearTimeout(timeoutId); // Cleanup timeout on component unmount
+    }
+  }, [currentIndex, imageUrls]); // Re-run effect when currentIndex or imageUrls change
 
   return (
     <div className="slideshow-container">
       {imageUrls.map((imageUrl, index) => (
         <img
-          key={Math.random(imageUrl)} // Assuming each imageUrl is unique
+          key={imageUrl} // Use imageUrl as the unique key
           src={imageUrl}
           alt={`Slideshow Art ${index + 1}`}
           className={index === currentIndex ? 'visible' : 'hidden'}
+          style={{
+            opacity: index === currentIndex ? 1 : 0,
+          }}
         />
       ))}
     </div>
