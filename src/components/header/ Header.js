8cbe90377d2645a81bc7aaeaaf64658eef345a6d
danieleugenevoigt <dvoigt@tutanota.com>
2024-05-04 11:42:04 -0600

    hamburger menu complete in header

--- src/components/header/ Header.js
+++ src/components/header/ Header.js
@@ -1,47 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter' || e.key === ' ') {
+      handleToggleMenu();
+    }
+  };
+
   return (
     <div className="headerMain">
       <h1>Lauren Voigt Fine Art</h1>
-      <ul className="navMenu">
-        <li>
-          <a href="/">
-            Home
-            {' '}
-          </a>
-        </li>
-        <li>
-          <a href="/gallery">
-            Gallery
-            {' '}
-          </a>
-        </li>
-        <li>
-          <a href="/about">
-            About
-            {' '}
-          </a>
-        </li>
-        <li>
-          <a href="/news">
-            News
-            {' '}
-          </a>
-        </li>
-        <li>
-          <a href="/events">
-            Events
-            {' '}
-          </a>
-        </li>
-        <li>
-          <a href="/contact">
-            Contact
-            {' '}
-          </a>
-        </li>
+      <div
+        className={`navToggle ${isMenuOpen ? 'open' : ''}`}
+        role="button"
+        tabIndex={0}
+        aria-label="button"
+        onClick={handleToggleMenu}
+        onKeyDown={handleKeyPress}
+      >
+        <div className="bar" />
+        <div className="bar" />
+        <div className="bar" />
+      </div>
+      <ul className={`navMenu ${isMenuOpen ? 'open' : ''}`}>
+        <li><a href="/">Home</a></li>
+        <li><a href="/gallery">Gallery</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/news">News</a></li>
+        <li><a href="/events">Events</a></li>
+        <li><a href="/contact">Contact</a></li>
       </ul>
     </div>
   );
