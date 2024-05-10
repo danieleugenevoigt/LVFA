63aa03bf4ac7a2c681f761b6f201a0f9d8e9cfee
daniel <daniel@pop-os.localdomain>
2024-05-10 12:31:04 -0600

    exhibitions page complete

--- src/components/exhibition/Exhibition.js
+++ src/components/exhibition/Exhibition.js
@@ -4,13 +4,23 @@ import './exhibition.css';
 function Exhibition() {
   return (
     <div className="eventsPageContent">
-      <h2>Upcoming Events</h2>
-      <div className="event">
-        <a href="https://www.evergreenfineartsfestival.com/">Evergreen Fine Arts Festival</a>
-        <p> August 23-24, 2024</p>
+      <div className="col1">
+        <h2>Upcoming Events</h2>
+        <div className="event">
+          <a href="https://www.evergreenfineartsfestival.com/">Evergreen Fine Arts Festival</a>
+          <p> Aug 23-24, 2024</p>
+          <p>10-5p</p>
+        </div>
+        <div className="eventDescription">
+          <p>Come see me at the Evergreen fine Arts Festival, booth #102 !</p>
+        </div>
       </div>
-      <div className="eventDescription">
-        <p>Come see me at the Evergreen fine Arts Festival, booth #102 !</p>
+      <div className="col2">
+        <h2>Viewing</h2>
+        <div className="event">
+          <a href="http://www.stoneheartgallery.com/home.html">Stoneheart Gallery</a>
+          <p> Jun 2024 - Current</p>
+        </div>
       </div>
     </div>
   );
