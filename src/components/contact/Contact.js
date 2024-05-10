76a8bfbfec9cd10dd281f1cbccdb31f93e0b2f99
daniel <daniel@pop-os.localdomain>
2024-05-10 16:24:43 -0600

    added contact basic css

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,74 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
+import {
+  Formik, Form, Field, ErrorMessage,
+} from 'formik';
+import * as Yup from 'yup';
+import './contact.css';
 
 function Contact() {
+  const initialValues = {
+    name: '',
+    email: '',
+    phone: '',
+    message: '',
+  };
+
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required('Name is required'),
+    email: Yup.string().email('Invalid email').required('Email is required'),
+    phone: Yup.string().matches(/^\+?[0-9]{8,14}$/, 'Invalid phone number'),
+    message: Yup.string().required('Message is required'),
+  });
+
+  const handleSubmit = (values, { resetForm }) => {
+    // Handle form submission here (e.g., send data to backend)
+    console.log(values);
+    resetForm();
+  };
+
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className="contactPageContent">
+      <p>Please contact me for gallery availablity, custom work, or other questions.</p>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ errors, touched }) => (
+          <Form className="formik">
+            <div>
+              <label htmlFor="name">Name:</label>
+              <Field type="text" id="name" name="name" />
+              <ErrorMessage name="name" component="div" className="error" />
+            </div>
+
+            <div>
+              <label htmlFor="email">Email:</label>
+              <Field type="email" id="email" name="email" />
+              <ErrorMessage name="email" component="div" className="error" />
+            </div>
+
+            <div>
+              <label htmlFor="phone">Phone:</label>
+              <Field type="text" id="phone" name="phone" />
+              <ErrorMessage name="phone" component="div" className="error" />
+            </div>
+
+            <div>
+              <label htmlFor="message">Message:</label>
+              <Field as="textarea" id="message" name="message" />
+              <ErrorMessage name="message" component="div" className="error" />
+            </div>
+
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
+
 export default Contact;
