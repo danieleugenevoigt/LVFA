d7e315aa1157a13458dc08b90f754111be113496
danieleugenevoigt <dvoigt@tutanota.com>
2024-06-22 15:12:46 -0600

    reworked content page to remove form and add images

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,73 +2,33 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
-import {
-  Formik, Form, Field, ErrorMessage,
-} from 'formik';
-import * as Yup from 'yup';
 import './contact.css';
 
 function Contact() {
-  const initialValues = {
-    name: '',
-    email: '',
-    phone: '',
-    message: '',
-  };
-
-  const validationSchema = Yup.object().shape({
-    name: Yup.string().required('Name is required'),
-    email: Yup.string().email('Invalid email').required('Email is required'),
-    phone: Yup.string().matches(/^\+?[0-9]{8,14}$/, 'Invalid phone number'),
-    message: Yup.string().required('Message is required'),
-  });
-
-  const handleSubmit = (values, { resetForm }) => {
-    // Handle form submission here (e.g., send data to backend)
-    console.log(values);
-    resetForm();
-  };
-
   return (
     <div className="contactPageContent">
-      <p>Please contact me for gallery availablity, custom work, or other questions.</p>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={handleSubmit}
-      >
-        {({ errors, touched }) => (
-          <Form className="formik">
-            <div>
-              <label htmlFor="name">Name:</label>
-              <Field type="text" id="name" name="name" />
-              <ErrorMessage name="name" component="div" className="error" />
-            </div>
-
-            <div>
-              <label htmlFor="email">Email:</label>
-              <Field type="email" id="email" name="email" />
-              <ErrorMessage name="email" component="div" className="error" />
-            </div>
-
-            <div>
-              <label htmlFor="phone">Phone:</label>
-              <Field type="text" id="phone" name="phone" />
-              <ErrorMessage name="phone" component="div" className="error" />
-            </div>
-
-            <div>
-              <label htmlFor="message">Message:</label>
-              <Field as="textarea" id="message" name="message" />
-              <ErrorMessage name="message" component="div" className="error" />
-            </div>
-            <div className="button-container">
-              <button type="submit">Submit</button>
-            </div>
-          </Form>
-        )}
-      </Formik>
+      <p>
+        For gallery availability, custom work, or other questions, email&nbsp;
+        <a
+          href="mailto:contact@laurenvoigtfineart.com"
+          aria-label="Email contact@laurenvoigtfineart.com"
+        >
+          contact@laurenvoigtfineart.com
+        </a>
+      </p>
+      <div className="imageCard">
+        <div className="artistPhoto">
+          <img src={`${process.env.PUBLIC_URL}/images/Lauren-Profile-Pic-1.jpeg`} alt="The Artist" />
+        </div>
+        <div className="artistWorkingPhoto">
+          <img src={`${process.env.PUBLIC_URL}/images/Lauren-Working.jpg`} alt="The Artist Working" />
+        </div>
+        <div className="artistWorkingDetailPhoto">
+          <img src={`${process.env.PUBLIC_URL}/images/Lauren-Working-Detail.jpg`} alt="The Artist Working Close Up" />
+        </div>
+      </div>
     </div>
+
   );
 }
 
